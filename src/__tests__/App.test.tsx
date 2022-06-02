32293c8aac6f01a039030ed1aac471c21d3504cf
Manuel Casanova <m.casa2017@outlook.com>
2022-06-02 18:34:40 -0400

    resolve bugs

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,10 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from '../renderer/App';
+import getPDF from '../renderer/services/getPDF';
 
 describe('App', () => {
   it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
+    expect(render(<App getPDF={getPDF} />)).toBeTruthy();
   });
 });
